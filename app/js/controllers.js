b8a60caa1ab4f564d49385d7359e558cd388efe8
Frank Sullivan <franks999@gmail.com>
2014-02-03 16:25:46 -0600

    misc

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -11,70 +11,40 @@ eventsApp.controller('EventListCtrl', function($scope, $http) {
   });
   $scope.model = [{
 		'id':'0',
-		'hood': 'Frenchman St'
+		'dist': 'Frenchman St'
 	},
 	{
 		'id':'1',
-		'hood': 'French Qtr'
+		'dist': 'French Qtr'
 	},
 	{
 		'id':'2',
-		'hood': 'Marigny'
+		'dist': 'Marigny'
 	},
 	{
 		'id':'3',
-		'hood': 'CBD'
+		'dist': 'CBD'
 	},
 	{
 		'id':'4',
-		'hood': 'Carrollton'
+		'dist': 'Carrollton'
 	},
 	{
 		'id':'5',
-		'hood': 'Uptown'
+		'dist': 'Uptown'
 	},
 	{
 		'id':'6',
-		'hood': 'Garden District'
+		'dist': 'Garden District'
 	},
 	{
 		'id':'7',
-		'hood': 'City Park'
+		'dist': 'City Park'
 	},
 		{
 		'id':'8',
-		'hood': 'Northshore'
+		'dist': 'Northshore'
 	}
 	];
 	$scope.selectedItem = $scope.model[0];  
 });
-
-eventsApp.controller("Gambit", ['$scope','FeedService', function($scope, GamFeed) {      
-	GamFeed.parseFeed('http://www.bestofneworleans.com/gambit/Rss.xml?section=1222776').then(function(res){
-		$scope.gambit = res.data.responseData.feed.entries;
-	});
-}]);
-
-eventsApp.controller("NewWWL", ['$scope','FeedService', function($scope, WWLFeed) {      
-	WWLFeed.parseFeed('http://events.wwltv.com/default.aspx?ct=r&ename=rsseventspage').then(function(res){
-		$scope.wwl = res.data.responseData.feed.entries;
-	});
-}]);
-
-eventsApp.controller("FeedCtrl", ['$scope','FeedService', function ($scope, Feed) {    
-	$scope.loadButonText="Load";
-	$scope.loadFeed=function(e){        
-		Feed.parseFeed($scope.feedSrc).then(function(res){
-			$scope.loadButonText=angular.element(e.target).text();
-			$scope.feeds=res.data.responseData.feed.entries;
-		});
-	};
-}]);
- 
-eventsApp.factory('FeedService',['$http',function($http){
-	return {
-		parseFeed : function(url){
-			return $http.jsonp('//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=' + encodeURIComponent(url));
-		}
-	};
-}]);
